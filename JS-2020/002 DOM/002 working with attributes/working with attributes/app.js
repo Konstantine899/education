26aa8f9870fc6ab0d142056ec1eb7e46515be156
Konstantine899 <kostay375298918971@gmail.ru>
2020-09-22 16:39:23 +0300

    014_js_2020

---
+++ JS-2020/002 DOM/002 working with attributes/working with attributes/app.js
@@ -0,0 +1,12 @@
+const div = document.querySelector("div");
+
+// div.classList.add("article", "custom");
+// div.classList.remove("article");
+// console.log(div.classList);
+// const link = div.querySelector(".link");
+// console.dir(link);
+
+div.setAttribute("id", "myId");
+// div.getAttribute("id");
+// div.hasAttribute("id");
+// div.removeAttribute("id");
