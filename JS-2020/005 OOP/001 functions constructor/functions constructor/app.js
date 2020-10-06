f315d6f67e491c454c959abbcc7e281c05c1f287
Konstantine899 <kostay375298918971@gmail.ru>
2020-10-06 15:16:06 +0300

    026_js_2020

---
+++ JS-2020/005 OOP/001 functions constructor/functions constructor/app.js
@@ -0,0 +1,17 @@
+// const str = new String("hello world");
+// console.log(str);
+
+function Product(brand, price, discount) {
+  // 1. Создается новый объект
+  // 2. На этот объект устанавливается ссылка this
+  // 3. Функция возвращает этот объект
+  this.brand = brand;
+  this.price = price;
+  this.discount = discount;
+  this.getPriceWithDiscount = function () {
+    return (this.price * (100 - this.discount)) / 100;
+  };
+}
+const apple = new Product("Apple", 100, 15);
+const samsung = new Product("Samsung", 200, 25);
+// console.log(apple, samsung);
